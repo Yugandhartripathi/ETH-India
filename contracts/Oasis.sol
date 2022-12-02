c91c05df51d344e3d8babdb3db795f885e0e5d02
YugandharTripathi <tripathi.yugandhar@gmail.com>
2022-12-03 03:14:15 +0530

    frontend boilerplate

---
+++ contracts/Oasis.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./Schema.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Oasis is ERC721URIStorage {}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import React,{useState} from "react";
+import Link from "next/link";
+
+import ReactDOM from "react-dom/client";
+import "./home.css";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
+
+import classes from "./../styles/app.module.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={classes.App}>
+      <MoralisProvider initializeOnMount={false}>
+        <NotificationProvider>
+          {/* <Link href="/"></Link> */}
+          <Component
+
+          />
+        </NotificationProvider>
+      </MoralisProvider>
+    </div>
+  );
 }
 
 export default MyApp
